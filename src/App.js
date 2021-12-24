9afb9e92d983f47c8c4598160f2ad00e9515dae6
Crunk <jb10051075@gmail.com>
2021-12-24 22:02:24 +0100

    shuffeld cards
    - Added starwars imges
    - Shuffeld cards with random number

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import {useState} from 'react'
 import './App.css';
 
+const cardImages = [
+  {"src":"/images/Bastila-Shan.jpg"},
+  {"src":"/images/Black-sith.jpg"},
+  {"src":"/images/Dark-lord-Vador.jpg"},
+  {"src":"/images/Darth-vador.jpg"},
+  {"src":"/images/Mandalorian.jpg"},
+  {"src":"/images/Yoda.jpg"}
+]
+
 function App() {
+  const [cards, setCards] = useState([])
+  const [turns, setTunrs] = useState(0)
+
+  // * Shuffle cards
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(()=> Math.random() - 0.5) // * negative number remains the same order | positive number switch the numbers around
+      .map((card) => ({...card, id:Math.random()})) // * Add ID proberty
+
+    setCards(shuffledCards)
+    setTunrs(0)
+  }
+    console.log(cards, turns)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Star Wars | Memory Game</h1>
+      <button onClick={shuffleCards}>New Game</button>
     </div>
   );
 }
