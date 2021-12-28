4c150aa191785b8ce7591f6ffb278c89e9ad9056
Crunk <jb10051075@gmail.com>
2021-12-28 19:30:06 +0100

    Card Grid
     Added cards in content
     Shuffelcards
     Displaying cards in grid

--- src/App.js
+++ src/App.js
@@ -1,35 +1,45 @@
-import {useState} from 'react'
+import { useState } from 'react';
 import './App.css';
 
 const cardImages = [
-  {"src":"/images/Bastila-Shan.jpg"},
-  {"src":"/images/Black-sith.jpg"},
-  {"src":"/images/Dark-lord-Vador.jpg"},
-  {"src":"/images/Darth-vador.jpg"},
-  {"src":"/images/Mandalorian.jpg"},
-  {"src":"/images/Yoda.jpg"}
-]
+	{ src: '/images/Bastila-Shan.jpg' },
+	{ src: '/images/Black-sith.jpg' },
+	{ src: '/images/Dark-lord-Vador.jpg' },
+	{ src: '/images/Darth-vador.jpg' },
+	{ src: '/images/Mandalorian.jpg' },
+	{ src: '/images/Yoda.jpg' },
+];
 
 function App() {
-  const [cards, setCards] = useState([])
-  const [turns, setTunrs] = useState(0)
+	const [cards, setCards] = useState([]);
+	const [turns, setTurn] = useState(0);
 
-  // * Shuffle cards
-  const shuffleCards = () => {
-    const shuffledCards = [...cardImages, ...cardImages]
-      .sort(()=> Math.random() - 0.5) // * negative number remains the same order | positive number switch the numbers around
-      .map((card) => ({...card, id:Math.random()})) // * Add ID proberty
+	// * Shuffle cards
+	const shuffleCards = () => {
+		const shuffledCards = [...cardImages, ...cardImages]
+			.sort(() => Math.random() - 0.5) // * negative number remains the same order | positive number switch the numbers around
+			.map((card) => ({ ...card, id: Math.random() })); // * Add ID proberty
 
-    setCards(shuffledCards)
-    setTunrs(0)
-  }
-    console.log(cards, turns)
-  return (
-    <div className="App">
-      <h1>Star Wars | Memory Game</h1>
-      <button onClick={shuffleCards}>New Game</button>
-    </div>
-  );
+		setCards(shuffledCards);
+		setTurn(0);
+	};
+	console.log(cards, turns);
+	return (
+		<div className='App'>
+			<h1>Star Wars | Memory Game</h1>
+			<button onClick={shuffleCards}>New Game</button>
+			<div className='card-grid'>
+				{cards.map((card) => (
+					<div className='card' key={card.id}>
+						<div>
+							<img className='front' src={card.src} alt='card front' />
+							<img className='back' src='/images/cover.jpg' alt='card cover' />
+						</div>
+					</div>
+				))}
+			</div>
+		</div>
+	);
 }
 
 export default App;
